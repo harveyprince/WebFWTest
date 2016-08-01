c8fb9abf318b87e35426d4af008fd3bbd21d8bfd
harveyprince <948841233@qq.com>
2016-08-02 00:34:24 +0800

    update

--- app/scripts/ang2/todo-input.ts
+++ app/scripts/ang2/todo-input.ts
@@ -1,4 +1,4 @@
-import {Component} from "angular2/core";
+import {Component, Inject} from "angular2/core";
 import {TodoService} from "./todo-service";
 
 @Component({
@@ -12,6 +12,7 @@ export class TodoInput{
     constructor(public todoService:TodoService){
 
     }
+
     onClick(value){
         this.todoService.todos.push(value);
         console.log(this.todoService.todos);
