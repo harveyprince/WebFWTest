96580a1d32e9e7dec9f17e2ab0b8bf70330f8d85
harveyprince <948841233@qq.com>
2016-08-04 10:00:24 +0800

    redux and react

--- app/scripts/Counter.pack.js
+++ app/scripts/Counter.pack.js
@@ -14,15 +14,39 @@ const counter = (state = 0, action) => {
             return state;
     }
 }
-
+class Counter extends React.Component{
+    render(){
+        const { value, onIncrement, onDecrement } = this.props
+        return (
+            <div>
+                <h2>{value}</h2>
+                <botton onClick={onIncrement}>+</botton>
+                <botton onClick={onDecrement}>-</botton>
+            </div>
+        )
+    }
+}
 const store = createStore(counter);
-
+class App extends React.Component {
+    render(){
+        return (
+            <Counter
+                value={store.getState()}
+                onIncrement={()=>{
+                    store.dispatch({type:'INCREMENT'})
+                }}
+                onDecrement={()=>{
+                    store.dispatch({type:'DECREMENT'})
+                }}
+            />
+        )
+    }
+}
 const render = () => {
-    document.body.innerText = store.getState();
+    ReactDOM.render(
+        <App/>,
+        document.getElementById('app')
+    );
 }
 store.subscribe(render);
 render();
-
-document.addEventListener('click',()=>{
-    store.dispatch({type:'INCREMENT'});
-});
